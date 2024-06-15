aa28d41605bbd9efb2768fb77d790ab3bb5d3830
user <user@localhost>
2024-06-15 10:22:26 -0500

    WIP

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,8 +8,9 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ENV_FILE="${SCRIPT_DIR}/../.env"
 
-docker run -it --rm \
+docker run -d -it --rm \
   --name "${NAME}" \
   -p 9222:9222 -v "${SCRIPT_DIR}/../nats.conf:/nats.conf" \
-  --env-file "${ENV_FILE}"  "${IMAGE_TAG}" \
+  --env-file "${ENV_FILE}" \
+   "${IMAGE_TAG}" \
   -c /nats.conf
